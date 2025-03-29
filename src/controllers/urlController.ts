a660f301377d54000df993c829fb4e28c7d150c9
Pool Llerena <98296929+Pool1541@users.noreply.github.com>
2025-03-29 17:15:19 +0000

    refactor: improve generateShortUrl function  and add getAllShortUrls controller

--- src/controllers/urlController.ts
+++ src/controllers/urlController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { sendMessage } from '../clients/kafkaClient';
+import { getAllUrls, getUrl } from '../clients/redisClient';
+import crypto from 'crypto';
 
 export const createShortUrl = async (req: Request, res: Response): Promise<void> => {
   const { originalUrl } = req.body;
@@ -28,6 +30,36 @@ export const registerClickEvent = async (req: Request, res: Response): Promise<v
   res.status(200).json({ message: 'Evento de clic registrado' });
 };
 
+export const getAllShortUrls = async (req: Request, res: Response ): Promise<void> => {
+  const urls = await getAllUrls();
+
+  if (!urls || urls.length === 0) {
+    res.status(404).json({ error: 'No se encontraron URLs acortadas' });
+    return;
+  };
+
+  res.status(200).json({ urls });
+}
+
+export const getOriginalUrl = async (req: Request, res: Response): Promise<void> => {
+  const { shortUrl } = req.params;
+
+  if (!shortUrl) {
+    res.status(400).json({ error: 'La URL acortada es requerida' });
+    return;
+  }
+
+  const originalUrl = await getUrl(shortUrl);
+
+  if (!originalUrl) {
+    res.status(404).json({ error: 'No se encontró la URL original para la URL acortada proporcionada' });
+    return;
+  }
+
+  res.status(200).json({ originalUrl });
+};
+
 const generateShortUrl = (originalUrl: string): string => {
-  return Buffer.from(originalUrl).toString('base64').slice(0, 8);
+  const hash = crypto.createHash('sha256').update(originalUrl).digest('base64');
+  return hash.replace(/[^a-zA-Z0-9]/g, '').slice(0, 8); // Eliminar caracteres no alfanuméricos y truncar
 };
