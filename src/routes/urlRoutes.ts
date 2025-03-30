a2a4c70d4cfa960414eebebe174f177b0a41428b
pool_1541 <pool_1541@hotmail.com>
2025-03-29 22:13:20 -0500

    feat: add route to retrieve click data for a shortened URL

--- src/routes/urlRoutes.ts
+++ src/routes/urlRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { createShortUrl, getAllShortUrls, getOriginalUrl, registerClickEvent } from '../controllers/urlController';
+import { createShortUrl, getAllClicks, getAllShortUrls, getOriginalUrl, registerClickEvent } from '../controllers/urlController';
 
 const router = Router();
 
 router.post('/shorten', createShortUrl);
-router.post('/click/:shortUrl', registerClickEvent);
+router.post('/clicks/:shortUrl', registerClickEvent);
 router.get('/:shortUrl', getOriginalUrl);
 router.get('/', getAllShortUrls);
+router.get('/clicks/:shortUrl',  getAllClicks)
 
 export default router;
